f7c440cdb8fc17aa3ab2435c14c7ddb8c055885c
TAHRI Anas <tahrianas@yahoo.fr>
2019-01-10 11:41:42 +0100

    Modify Database Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Role;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+    	$format = 'Y-m-d H:i:s';
+    	DB::table('users')->insert([
+            'name' => 'Tahri Anas',
+            'email' => 'tahrianas@yahoo.fr',
+            'username' => 'admin',
+            'password' => bcrypt('admin'),
+            'created_at' => date($format),
+            'updated_at' => date($format)
+        ]);
+
+        DB::table('roles')->insert([
+            'name' => 'admin',
+            'label' => 'Administrator',
+            'created_at' => date($format),
+            'updated_at' => date($format)
+        ]);
+
+        $user_id = DB::table('users')->where('username', 'admin')->value('id');
+
+        $role_id = DB::table('roles')->where('name', 'admin')->value('id');
+
+        DB::table('role_user')->insert([
+            'role_id' => $role_id,
+            'user_id' => $user_id
+        ]);
     }
 }
